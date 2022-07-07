6ff57c401afc7c7eee2b779d2645cb48a52eb750
Okoli Nnaemeka <okolinnaemeka227@gmail.com>
2022-07-07 02:59:40 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((error) => console.error("Could not connect to mongodb...", error));
+
+app.use(express.json());
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}....`));
